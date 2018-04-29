e834801ffb8d62ff554e169e4ed1f373d7defc18
NMonastero <33333449+NMonastero@users.noreply.github.com>
2018-04-29 12:45:41 -0400

    Up until JUnit Test is done

--- src/project/MachineModel.java
+++ src/project/MachineModel.java
@@ -17,6 +17,92 @@ public class MachineModel {
 		withGUI = b;
 		callback = h;
 
+		//INSTRUCTION_MAP entry "No operation"
+		INSTRUCTIONS.put(0x0, arg -> {
+			cpu.incrementIP(1);
+		});
+
+		//INSTRUCTION_MAP entry for "LODI"
+		INSTRUCTIONS.put(0x1, arg -> {
+			cpu.accumulator = arg;
+			cpu.incrementIP(1);
+		});
+
+		//INSTRUCTION_MAP entry for "LOD"
+		INSTRUCTIONS.put(0x2, arg -> {
+			int arg1 = memory.getData(cpu.memoryBase+arg);
+			cpu.accumulator = arg1;
+			cpu.incrementIP(1);
+		});
+
+		//INSTRUCTION_MAP entry for "LODN"
+		INSTRUCTIONS.put(0x3, arg -> {
+			int arg1 = memory.getData(cpu.memoryBase+arg);
+			int arg2 = memory.getData(cpu.memoryBase+arg1);
+			cpu.accumulator = arg2;
+			cpu.incrementIP(1);
+		});
+
+		//INSTRUCTION_MAP entry for "STO"
+		INSTRUCTIONS.put(0x4, arg -> {
+			memory.setData(cpu.memoryBase+arg, cpu.accumulator);
+			cpu.incrementIP(1);
+		});
+
+		//INSTRUCTION_MAP entry for "STON"
+		INSTRUCTIONS.put(0x5, arg -> {
+			int arg1 = memory.getData(cpu.memoryBase+arg);
+			memory.setData(cpu.memoryBase+arg1, cpu.accumulator);
+			cpu.incrementIP(1);
+		});
+
+		//INSTRUCTOIN_MAP entry for "JUMPR"
+		INSTRUCTIONS.put(0x6,  arg -> {
+			cpu.incrementIP(arg);
+		});
+
+		//INSTRUCTOIN_MAP entry for "JUMP"
+		INSTRUCTIONS.put(0x7,  arg -> {
+			cpu.incrementIP(cpu.memoryBase+arg);
+		});
+
+		//INSTRUCTOIN_MAP entry for "JUMPI"
+		INSTRUCTIONS.put(0x8,  arg -> {
+			cpu.incrementIP(cpu.instructionPointer*-1);
+			cpu.incrementIP(arg);
+		});
+
+		//INSTRUCTOIN_MAP entry for "JMPZR"
+		INSTRUCTIONS.put(0x9,  arg -> {
+			if(cpu.accumulator == 0) {
+				cpu.incrementIP(arg);
+			}
+			else {
+				cpu.incrementIP(1);
+			}
+		});
+
+		//INSTRUCTOIN_MAP entry for "JMPZ"
+		INSTRUCTIONS.put(0xA,  arg -> {
+			if(cpu.accumulator == 0) {
+				cpu.incrementIP(cpu.memoryBase+arg);
+			}
+			else {
+				cpu.incrementIP(1);
+			}
+		});
+
+		//INSTRUCTOIN_MAP entry for "JUMPZI"
+		INSTRUCTIONS.put(0xB,  arg -> {
+			if(cpu.accumulator == 0) {
+				cpu.incrementIP(cpu.instructionPointer*-1);
+				cpu.incrementIP(arg);
+			}
+			else {
+				cpu.incrementIP(1);
+			}
+		});
+
 		//INSTRUCTION_MAP entry for "ADDI"
 		INSTRUCTIONS.put(0xC, arg -> {
 			cpu.accumulator += arg;
@@ -109,19 +195,114 @@ public class MachineModel {
 			cpu.accumulator /= arg2;
 			cpu.incrementIP(1);
 		});
+
+		//INSTRUCTION_MAP entry for "ANDI"
+		INSTRUCTIONS.put(0x18, arg -> {
+			if(cpu.accumulator != 0 && arg != 0) {
+				cpu.accumulator = 1;
+			}
+			else {
+				cpu.accumulator = 0;
+			}
+			cpu.incrementIP(1);
+		});
+
+		//INSTRUCTION_MAP entry for "AND"
+		INSTRUCTIONS.put(0x19, arg -> {
+			if(cpu.accumulator != 0 && cpu.memoryBase+arg != 0) {
+				cpu.accumulator = 1;
+			}
+			else {
+				cpu.accumulator = 0;
+			}
+			cpu.incrementIP(1);
+		});
+
+		//INSTRUCTION_MAP entry for "NOT"
+		INSTRUCTIONS.put(0x1A, arg -> {
+			if(cpu.accumulator != 0) {
+				cpu.accumulator = 0;
+			}
+			else {
+				cpu.accumulator = 1;
+			}
+			cpu.incrementIP(1);
+		});
+
+		//INSTRUCTION_MAP entry for "CMPL"
+		INSTRUCTIONS.put(0x1B, arg -> {
+			if(cpu.memoryBase < 0) {
+				cpu.accumulator = 1;
+			}
+			else {
+				cpu.accumulator = 0;
+			}
+			cpu.incrementIP(1);
+		});
+
+		//INSTRUCTION_MAP entry for "CMPZ"
+		INSTRUCTIONS.put(0x1C, arg -> {
+			if(cpu.memoryBase == 0) {
+				cpu.accumulator = 1;
+			}
+			else {
+				cpu.accumulator = 0;
+			}
+			cpu.incrementIP(1);
+		});
+
+		//INSTRUCTION_MAP entry for "HALT"
+		INSTRUCTIONS.put(0x1F, arg -> {
+			callback.halt();
+		});
+	}
+	
+	int[] getData() {
+		return memory.getData();
+	}
+	
+	public int getData(int index) {
+		return memory.getData(index);
+	}
+	
+	public void setData(int index, int value) {
+		memory.setData(index, value);
+	}
+	
+	public int getAccumulator() {
+		return cpu.accumulator;
 	}
 
+	public void setAccumulator(int accumulator) {
+		cpu.accumulator = accumulator;
+	}
+
+	public int getInstructionPointer() {
+		return cpu.instructionPointer;
+	}
+
+	public void setInstructionPointer(int instructionPointer) {
+		cpu.instructionPointer = instructionPointer;
+	}
+
+	public int getMemoryBase() {
+		return cpu.memoryBase;
+	}
+
+	public void setMemoryBase(int memoryBase) {
+		cpu.memoryBase = memoryBase;
+	}
+	
 	public class CPU{
 		//the getters and setters for these should be in machineModel
 		private int accumulator;
 		private int instructionPointer;
 		private int memoryBase;
 
-		//something in the instructions about this incrementing by one if you don't have int val
 		public void incrementIP(int val) {
 			instructionPointer += val;
 		}
-
+		
 
 		//The instructions will be stored in code-memory which is also an array stored in Memory. 
 		//The plan is to be able to run more than one program in "parallel"
